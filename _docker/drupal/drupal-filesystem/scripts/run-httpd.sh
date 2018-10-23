3a0448c2720ae0782ae774263ef085c95e36a41f
robpblake <rob@rb-associates.co.uk>
2018-10-23 14:23:07 +0100

    [RHDENG-2482] First attempts at implementing Jenkinsfile Pipeline for Deployment on Openshift

--- _docker/drupal/drupal-filesystem/scripts/run-httpd.sh
+++ _docker/drupal/drupal-filesystem/scripts/run-httpd.sh
@@ -3,51 +3,8 @@ set -e
 
 trap "apachectl -k graceful-stop" TERM INT
 
-cd /var/www/drupal
-
 # This sleep seems to be required for certain Docker/OS combos as the bind mounts may not be fully ready
 sleep 10
-
-# Check if drupal is installed, install if needed
-ruby drupal_install_checker.rb
-
-# Build theme
-cd /var/www/drupal/web/themes/custom/rhdp/rhd-frontend
-npm config set @fortawesome:registry ${FORTAWESOME_REGISTRY}
-
-# Add npm proxy
-if [ -z ${http_proxy+x} ]
-then
-  echo "No proxy config"
-else
-  echo "Setting npm proxy to http://${http_proxy}"
-  npm config set proxy http://${http_proxy}
-fi
-if [ -z ${https_proxy+x} ]
-then
-  echo "No proxy config"
-else
-  echo "Setting npm https-proxy to http://${https_proxy}"
-  npm config set https-proxy http://${https_proxy}
-fi
-
-npm install
-npm run-script build
-
-# Go back to where we were before
-cd /var/www/drupal
-
-# Run all database migrations
-ruby /var/www/drupal/phinx.rb migrate
-
-# Make sure we're not confused by old, incompletely-shutdown httpd
-# context after restarting the container.  httpd won't start correctly
-# if it thinks it is already running.
-rm -rf /run/httpd/*
-
-# Set it up so apache can write to everything
-chown -R apache:apache /var/www/drupal/web/sites
-chown -R apache:apache /var/www/drupal/web/config/active # just to make sure we can write changes to active
 
 # Launch Apache as a child-process of this script so we can 'wait' on it
 /usr/sbin/httpd -D FOREGROUND &
