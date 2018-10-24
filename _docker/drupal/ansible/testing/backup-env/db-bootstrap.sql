4d3b3e4af3448a96fde03c824061ba627e32dead
robpblake <rob@rb-associates.co.uk>
2018-10-24 13:19:21 +0100

    [RHDENG-2482] Moved Ansible playbooks into build of Drupal Docker Image

---
+++ _docker/drupal/ansible/testing/backup-env/db-bootstrap.sql
@@ -0,0 +1,3 @@
+use drupal_1;
+create table foo (hello_world varchar(255));
+insert into foo values ('this is a test of the communication system');
---
+++ _docker/drupal/ansible/testing/cleanup-env/db-bootstrap.sql
@@ -0,0 +1,4 @@
+create database drupal_1;
+create database drupal_2;
+create database drupal_3;
+create database drupal_4;
