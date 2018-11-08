aed229ef9cdb00881f9eabd9bba4caf330cb6a5b
robpblake <rob@rb-associates.co.uk>
2018-11-08 15:55:02 +0000

    [RHDENG-2472] Moved to an SCL based install on RHEL 7 for MP support

--- _docker/drupal/drupal-filesystem/scripts/run-httpd.sh
+++ _docker/drupal/drupal-filesystem/scripts/run-httpd.sh
@@ -7,16 +7,16 @@ trap "apachectl -k graceful-stop" TERM INT
 sleep 10
 
 # Launch Apache as a child-process of this script so we can 'wait' on it
-/usr/sbin/httpd -D FOREGROUND &
+httpd -D FOREGROUND &
 
 # Wait for the Apache pid file to be populated
-until [ -e '/etc/httpd/run/httpd.pid' ]; do
+until [ -e '/opt/rh/httpd24/root/etc/httpd/run/httpd.pid' ]; do
   sleep 5
-  echo "Waiting for Drupal PID file at '/etc/httpd/run/httpd.pid'"
+  echo "Waiting for Drupal PID file at '/opt/rh/httpd24/root/etc/httpd/run/httpd.pid'"
 done
 
 # Read the pid that Apache is running as
-PID=$(cat /etc/httpd/run/httpd.pid)
+PID=$(cat /opt/rh/httpd24/root/etc/httpd/run/httpd.pid)
 echo "Success: Drupal is running as PID '$PID'"
 
 # We now wait for as long as Apache is running
