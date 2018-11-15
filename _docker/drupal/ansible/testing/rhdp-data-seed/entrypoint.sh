669c343c317c02f7b019cabda848d92e97eeb4c5
robpblake <rob@rb-associates.co.uk>
2018-11-15 16:44:31 +0000

    [RHDENG-2630] Fixed attempted Drupal boot if database import fails

--- _docker/drupal/ansible/testing/rhdp-data-seed/entrypoint.sh
+++ _docker/drupal/ansible/testing/rhdp-data-seed/entrypoint.sh
@@ -1,7 +1,8 @@
 # Used to see the demonstrator environment with data from the most recent backup of production.
 #!/bin/sh
-mkdir -p "/drupal-workspace/drupal_$DEPLOYMENT_ID/drupal/config" "/drupal-workspace/drupal_$DEPLOYMENT_ID/seed/db"
+mkdir -p "/drupal-workspace/drupal_$DEPLOYMENT_ID/drupal/config/active" "/drupal-workspace/drupal_$DEPLOYMENT_ID/seed/db"
 cp "/docker-entrypoint-initdb.d/drupal-db.sql.gz" "/drupal-workspace/drupal_$DEPLOYMENT_ID/seed/db/drupal-db-sql.gz"
-cp -r "/var/www/drupal/web/config/active" "/drupal-workspace/drupal_$DEPLOYMENT_ID/drupal/config"
+cp -r /var/www/drupal/web/config/active/* "/drupal-workspace/drupal_$DEPLOYMENT_ID/drupal/config/active"
 cp -r "/var/www/drupal/web/sites/default/files" "/drupal-workspace/drupal_$DEPLOYMENT_ID/drupal"
+chgrp -R root "/drupal-workspace/drupal_$DEPLOYMENT_ID"
 chmod -R 775 "/drupal-workspace/drupal_$DEPLOYMENT_ID"
