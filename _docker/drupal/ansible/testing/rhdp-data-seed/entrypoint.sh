26f83781959c6e18cc19610b497d473880117e9e
robpblake <rob@rb-associates.co.uk>
2018-12-05 17:46:55 +0000

    [RHDENG-2476] [RHDENG-2475] [RHDENG-2481] Various pieces of work to ensure that we can properly seed and then destroy a drupal deployment

--- _docker/drupal/ansible/testing/rhdp-data-seed/entrypoint.sh
+++ _docker/drupal/ansible/testing/rhdp-data-seed/entrypoint.sh
@@ -1,8 +1,16 @@
 # Used to see the demonstrator environment with data from the most recent backup of production.
 #!/bin/sh
-mkdir -p "/drupal-workspace/drupal_$DEPLOYMENT_ID/drupal/config/active" "/drupal-workspace/drupal_$DEPLOYMENT_ID/seed/db"
-cp "/docker-entrypoint-initdb.d/drupal-db.sql.gz" "/drupal-workspace/drupal_$DEPLOYMENT_ID/seed/db/drupal-db-sql.gz"
-cp -r /var/www/drupal/web/config/active/* "/drupal-workspace/drupal_$DEPLOYMENT_ID/drupal/config/active"
-cp -r "/var/www/drupal/web/sites/default/files" "/drupal-workspace/drupal_$DEPLOYMENT_ID/drupal"
-chgrp -R root "/drupal-workspace/drupal_$DEPLOYMENT_ID"
-chmod -R 775 "/drupal-workspace/drupal_$DEPLOYMENT_ID"
+set -ex
+if [ ! -d "/drupal-workspace/drupal_$DEPLOYMENT_ID/drupal/config/active" ]
+then
+    BACKUP_DATE=`date -u '+%Y-%m-%d-%H-%M-%S'`
+    BACKUP_DIR="/drupal-backups/drupal_$DEPLOYMENT_ID-$BACKUP_DATE"
+    mkdir -p "$BACKUP_DIR"
+    cp "/docker-entrypoint-initdb.d/drupal-db.sql.gz" "$BACKUP_DIR/drupal-db.sql.gz"
+    cd /var/www/drupal/web
+    tar czf "$BACKUP_DIR/drupal-filesystem.tar.gz" config sites
+    chgrp -R root "$BACKUP_DIR"
+    chmod -R 775 "$BACKUP_DIR"
+else
+    echo "Seed of environment has already been completed"
+fi
