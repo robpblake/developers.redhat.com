42ea898130b11585906bcfe7620ec525a69b1b6d
robpblake <rob@rb-associates.co.uk>
2019-01-19 17:59:06 +0000

    [RHDENG-1679] Further work to improve local dev experience

---
+++ _docker/drupal/dev/run-drupal.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -e
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+cd $DIR && docker-compose down -v
+cd $DIR && docker-compose build --pull
+cd $DIR && mkcert localhost
+cd $DIR && docker-compose run --rm seed_env
+cd $DIR && docker-compose run --rm bootstrap_env
+cd $DIR && docker-compose run --rm bootstrap_drupal
+cd $DIR && chmod -R 777 ./drupal-workspace/drupal_1/drupal/sites/default/files
+cd $DIR && docker-compose up -d drupal
+cd $DIR && docker-compose logs -f drupal
