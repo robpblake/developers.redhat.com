5b4349644246e66055f9786d27c19003c15ae4b1
robpblake <rob@rb-associates.co.uk>
2019-01-19 12:55:04 +0000

    [RHDENG-2792] Integrated backups of Drupal with S3

--- _docker/drupal/managed-platform/ansible/testing/rhdp-data-seed/entrypoint.sh
+++ _docker/drupal/managed-platform/ansible/testing/rhdp-data-seed/entrypoint.sh
@@ -4,14 +4,13 @@ set -ex
 rm -rf /drupal-workspace/drupal_$DEPLOYMENT_ID/*
 if [ ! -d "/drupal-workspace/drupal_$DEPLOYMENT_ID/drupal/config/active" ]
 then
-    BACKUP_DATE=`date -u '+%Y-%m-%d-%H-%M-%S'`
-    BACKUP_DIR="/drupal-backups/drupal_$DEPLOYMENT_ID-$BACKUP_DATE"
-    mkdir -p "$BACKUP_DIR"
-    cp "/docker-entrypoint-initdb.d/drupal-db.sql.gz" "$BACKUP_DIR/drupal-db.sql.gz"
+    WORK_DIR="/drupal-workspace/drupal_$DEPLOYMENT_ID/work"
+    mkdir -p "$WORK_DIR"
+    cp "/docker-entrypoint-initdb.d/drupal-db.sql.gz" "$WORK_DIR/drupal-db.sql.gz"
     cd /var/www/drupal/web
-    tar czf "$BACKUP_DIR/drupal-filesystem.tar.gz" config sites
-    chgrp -R root "$BACKUP_DIR"
-    chmod -R 775 "$BACKUP_DIR"
+    tar czf "$WORK_DIR/drupal-filesystem.tar.gz" config sites
+    chgrp -R root "$WORK_DIR"
+    chmod -R 775 "$WORK_DIR"
 else
     echo "Seed of environment has already been completed"
 fi
