5fe803f8a61af70b3448955a7ba96735be6a98f4
robpblake <rob@rb-associates.co.uk>
2019-01-22 12:48:04 +0000

    [RHDENG-2646] Updated Drupal login page for login via developer accounts

--- _docker/drupal/dev/run-composer-update.sh
+++ _docker/drupal/dev/run-composer-update.sh
@@ -3,4 +3,5 @@ set -e
 echo "Be patient..this will take time..."
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
+cd $DIR && docker-compose build
 cd $DIR && docker-compose run --rm update-composer
