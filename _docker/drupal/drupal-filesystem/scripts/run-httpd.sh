811a063df708ce8b804a6492bae0264ea6dd8f5d
robpblake <rob@rb-associates.co.uk>
2019-02-01 13:30:09 +0000

    Reverted some changes back to master

--- _docker/drupal/drupal-filesystem/scripts/run-httpd.sh
+++ _docker/drupal/drupal-filesystem/scripts/run-httpd.sh
@@ -3,22 +3,63 @@ set -e
 
 trap "apachectl -k graceful-stop" TERM INT
 
+cd /var/www/drupal
+
 # This sleep seems to be required for certain Docker/OS combos as the bind mounts may not be fully ready
 sleep 10
 
-rm -rf /opt/rh/httpd24/root/var/run/httpd/*
+# Check if drupal is installed, install if needed
+ruby drupal_install_checker.rb
+
+# Build theme
+cd /var/www/drupal/web/themes/custom/rhdp/rhd-frontend
+npm config set @fortawesome:registry ${FORTAWESOME_REGISTRY}
+
+# Add npm proxy
+if [ -z ${http_proxy+x} ]
+then
+  echo "No proxy config"
+else
+  echo "Setting npm proxy to http://${http_proxy}"
+  npm config set proxy http://${http_proxy}
+fi
+if [ -z ${https_proxy+x} ]
+then
+  echo "No proxy config"
+else
+  echo "Setting npm https-proxy to http://${https_proxy}"
+  npm config set https-proxy http://${https_proxy}
+fi
+
+npm install
+npm run-script build
+
+# Go back to where we were before
+cd /var/www/drupal
+
+# Run all database migrations
+ruby /var/www/drupal/phinx.rb migrate
+
+# Make sure we're not confused by old, incompletely-shutdown httpd
+# context after restarting the container.  httpd won't start correctly
+# if it thinks it is already running.
+rm -rf /run/httpd/*
+
+# Set it up so apache can write to everything
+chown -R z:apache /var/www/drupal/web/sites
+chown -R apache:apache /var/www/drupal/web/config/active # just to make sure we can write changes to active
 
 # Launch Apache as a child-process of this script so we can 'wait' on it
-httpd -D FOREGROUND &
+/usr/sbin/httpd -D FOREGROUND &
 
 # Wait for the Apache pid file to be populated
-until [ -e '/opt/rh/httpd24/root/var/run/httpd/httpd.pid' ]; do
+until [ -e '/etc/httpd/run/httpd.pid' ]; do
   sleep 5
-  echo "Waiting for Drupal PID file at '/opt/rh/httpd24/root/var/run/httpd/httpd.pid'"
+  echo "Waiting for Drupal PID file at '/etc/httpd/run/httpd.pid'"
 done
 
 # Read the pid that Apache is running as
-PID=$(cat /opt/rh/httpd24/root/var/run/httpd/httpd.pid)
+PID=$(cat /etc/httpd/run/httpd.pid)
 echo "Success: Drupal is running as PID '$PID'"
 
 # We now wait for as long as Apache is running
