df883f43eba44951de9bfcf20cc0dded5ecabbea
robpblake <rob@rb-associates.co.uk>
2019-02-06 14:53:18 +0000

    [MWES-2857] Updated Drupal to use PHP-FPM to see if we can squeeze some more performance out of our MP deployment

--- _docker/drupal/managed-platform/var/www/drupal/run-httpd.sh
+++ _docker/drupal/managed-platform/var/www/drupal/run-httpd.sh
@@ -3,6 +3,18 @@ set -e
 
 trap "apachectl -k graceful-stop" TERM INT
 
+# Start up php-fpm and ensure that we have some processes running
+/opt/rh/rh-php71/root/usr/sbin/php-fpm -R&
+FPM_PROCESSES=$(ps -U apache | grep php-fpm | wc -l)
+
+until [ "$FPM_PROCESSES" -gt "0" ]; do
+    sleep 5
+    echo "Waiting for PHP-FPM processes to start..."
+    FPM_PROCESSES=$(ps -U apache | grep php-fpm | wc -l)
+done
+
+echo "'$FPM_PROCESSES' PHP-FPM processes running for Drupal under Apache user."
+
 rm -rf /opt/rh/httpd24/root/var/run/httpd/*
 
 # Launch Apache as a child-process of this script so we can 'wait' on it
