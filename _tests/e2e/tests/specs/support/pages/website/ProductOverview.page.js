f0989b8b2b167146028cd617e78618976ee15f71
robpblake <rob@rb-associates.co.uk>
2019-11-14 15:14:57 +0000

    [MWES-3564] Final tweaks to get everything to work

--- _tests/e2e/tests/specs/support/pages/website/ProductOverview.page.js
+++ _tests/e2e/tests/specs/support/pages/website/ProductOverview.page.js
@@ -2,20 +2,17 @@ import Page from '../Page';
 import Driver from '../../utils/Driver.Extension';
 
 class ProductOverview extends Page {
-    get allDownloads() {return $('//h4[contains(.,"All Downloads")]');}
     get downloadBtn() {return $('//rhdp-os-download/div/a');}
     get downloadThankYou() {return $('#downloadthankyou');}
 
     open(productCode, tab) {
         super.open(`/products/${productCode}/${tab}`.toString());
-        Driver.awaitIsDisplayed(this.allDownloads)
-        Driver.screenShot('download-page.png')
+        Driver.awaitIsDisplayed(this.downloadBtn)
         return this;
     }
 
     download() {
         Driver.click(this.downloadBtn);
-        Driver.screenShot('where-are-we.png');
         return this;
     }
 
