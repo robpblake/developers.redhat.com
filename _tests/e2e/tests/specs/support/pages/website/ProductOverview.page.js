1194b5dacd715854eab5e1379e6c51b84dad7a70
robpblake <rob@rb-associates.co.uk>
2019-11-14 13:11:05 +0000

    [MWES-3564] Download tests are not working on the new infra

--- _tests/e2e/tests/specs/support/pages/website/ProductOverview.page.js
+++ _tests/e2e/tests/specs/support/pages/website/ProductOverview.page.js
@@ -2,12 +2,13 @@ import Page from '../Page';
 import Driver from '../../utils/Driver.Extension';
 
 class ProductOverview extends Page {
+    get allDownloads() {return $('//h4[contains(.,"All Downloads")]');}
     get downloadBtn() {return $('//rhdp-os-download/div/a');}
     get downloadThankYou() {return $('#downloadthankyou');}
 
     open(productCode, tab) {
         super.open(`/products/${productCode}/${tab}`.toString());
-        Driver.awaitIsDisplayed(this.downloadBtn)
+        Driver.awaitIsDisplayed(this.allDownloads)
         return this;
     }
 
